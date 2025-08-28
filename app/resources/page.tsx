3c51f84cc02dcd9a9a05ea42735cf1621b22572b
mac <hassanjawad1321@gmail.com>
2025-08-29 01:50:04 +0500

    Updated image links

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -14,77 +14,77 @@ export default function ResourcesPage() {
     {
       id: 1,
       title: "Top 20 Cambridge GP Topics",
-      image: "https://img.notionusercontent.com/s3/prod-files-secure%2F0e28e540-810b-4715-b51f-c1ec5f175ba5%2F9a8cb61a-58f0-40bf-9c28-c8ce980616e6%2FChatGPT_Image_Jul_29_2025_04_24_27_PM.png/size/w=380?exp=1755681809&sig=O24pl0syt8TMwEKV5cxmalzokDxrrA93ySaaJDTDjtQ&id=23f0d3e2-bb6d-806f-b98c-dd8d108cc53e&table=block",
+      image: "https://file.notion.so/f/f/0e28e540-810b-4715-b51f-c1ec5f175ba5/9a8cb61a-58f0-40bf-9c28-c8ce980616e6/ChatGPT_Image_Jul_29_2025_04_24_27_PM.png?table=block&id=23f0d3e2-bb6d-806f-b98c-dd8d108cc53e&spaceId=0e28e540-810b-4715-b51f-c1ec5f175ba5&expirationTimestamp=1756440000000&signature=aUM3GQsv6CWnNy0UwbdiCDJCDWA_6lJDei9PmYWZrVQ&downloadName=ChatGPT+Image+Jul+29%2C+2025%2C+04_24_27+PM.png" ,
       category: "Essay Topics",
       pdfUrl: "https://tcsuimqcjsqbfjnoxlxo.supabase.co/storage/v1/object/public/Books%20Novus%20Education/Top%2020%20Cambridge%20GP%20topics.pdf"
     },
     {
       id: 2,
       title: "Mid Band to Band 1 Essay Samples",
-      image: "https://img.notionusercontent.com/s3/prod-files-secure%2F0e28e540-810b-4715-b51f-c1ec5f175ba5%2Fe5ce3328-eeb9-41ae-8242-85b7e843ef4d%2FChatGPT_Image_Jul_29_2025_04_29_52_PM.png/size/w=380?exp=1755681812&sig=FtiJpLEKsQI8gmgcLrZvkqb2lh0JyagpFOORrfiuHEM&id=23f0d3e2-bb6d-807e-8e07-cfb0cc66ae7c&table=block",
+      image: "https://file.notion.so/f/f/0e28e540-810b-4715-b51f-c1ec5f175ba5/e5ce3328-eeb9-41ae-8242-85b7e843ef4d/ChatGPT_Image_Jul_29_2025_04_29_52_PM.png?table=block&id=23f0d3e2-bb6d-807e-8e07-cfb0cc66ae7c&spaceId=0e28e540-810b-4715-b51f-c1ec5f175ba5&expirationTimestamp=1756440000000&signature=_NYlbjeBTO_Mzza-DqLjhqTauLT8PC2CXZBdiMtF8sU&downloadName=ChatGPT+Image+Jul+29%2C+2025%2C+04_29_52+PM.png",
       category: "Essay Samples",
       pdfUrl: "https://tcsuimqcjsqbfjnoxlxo.supabase.co/storage/v1/object/public/Books%20Novus%20Education/A%20level%20general%20paper%20(GP)%20from%20mid%20Band%20to%20Band%201%20before%20and%20after%20essay%20samples%20.pdf"
     },
     {
       id: 3,
       title: "Score Booster Phrases for GP Essays",
-      image: "https://img.notionusercontent.com/s3/prod-files-secure%2F0e28e540-810b-4715-b51f-c1ec5f175ba5%2F72dde876-3f68-4e00-adab-7410deaa0537%2FChatGPT_Image_Jul_29_2025_04_36_18_PM.png/size/w=380?exp=1755681817&sig=kRGsQGvLJghgIv0Dgl5WsY-lT70_xiypN762CyeI4Ag&id=23f0d3e2-bb6d-809f-84fb-f55fccfad48a&table=block",
+      image: "https://file.notion.so/f/f/0e28e540-810b-4715-b51f-c1ec5f175ba5/72dde876-3f68-4e00-adab-7410deaa0537/ChatGPT_Image_Jul_29_2025_04_36_18_PM.png?table=block&id=23f0d3e2-bb6d-809f-84fb-f55fccfad48a&spaceId=0e28e540-810b-4715-b51f-c1ec5f175ba5&expirationTimestamp=1756440000000&signature=B1SBC2Qu-8sTqknm96xXqp1-qRuES_lu4cjLpTXdwPY&downloadName=ChatGPT+Image+Jul+29%2C+2025%2C+04_36_18+PM.png",
       category: "Essay Writing",
       pdfUrl: "https://tcsuimqcjsqbfjnoxlxo.supabase.co/storage/v1/object/public/Books%20Novus%20Education/Score%20booster%20phrases%20for%20GP%20essays.pdf"
     },
     {
       id: 4,
       title: "15 Arguments Templates for Any Essay Question",
-      image: "https://img.notionusercontent.com/s3/prod-files-secure%2F0e28e540-810b-4715-b51f-c1ec5f175ba5%2Ff265c776-1e1b-4139-858d-df581765e7fe%2FChatGPT_Image_Jul_29_2025_04_38_16_PM.png/size/w=380?exp=1755681819&sig=vC6RgdxTDJFTYLXJs2XqqevTffltJAZuEQsCtqLMkhk&id=23f0d3e2-bb6d-808f-8087-e39f413b47d5&table=block",
+      image: "https://file.notion.so/f/f/0e28e540-810b-4715-b51f-c1ec5f175ba5/f265c776-1e1b-4139-858d-df581765e7fe/ChatGPT_Image_Jul_29_2025_04_38_16_PM.png?table=block&id=23f0d3e2-bb6d-808f-8087-e39f413b47d5&spaceId=0e28e540-810b-4715-b51f-c1ec5f175ba5&expirationTimestamp=1756440000000&signature=_ZKOAf_2uGufKaI6arYkrPTljys5a6KFj5anrlzi6pM&downloadName=ChatGPT+Image+Jul+29%2C+2025%2C+04_38_16+PM.png",
       category: "Essay Templates",
       pdfUrl: "https://tcsuimqcjsqbfjnoxlxo.supabase.co/storage/v1/object/public/Books%20Novus%20Education/15%20arguments%20templates%20for%20any%20essay%20question.pdf"
     },
     {
       id: 5,
       title: "A Level Score-Boosting Phrases for GP Essays",
-      image: "https://img.notionusercontent.com/s3/prod-files-secure%2F0e28e540-810b-4715-b51f-c1ec5f175ba5%2F8d4e3e7a-7548-43ae-b89c-ae7f3c3ca991%2FChatGPT_Image_Jul_29_2025_04_40_06_PM.png/size/w=380?exp=1755681821&sig=Tv_-zq20CfGsMD1uJZTt_mor3KySha0WU0r1kDVyxCs&id=23f0d3e2-bb6d-809c-b004-f23c0e075675&table=block",
+      image: "https://file.notion.so/f/f/0e28e540-810b-4715-b51f-c1ec5f175ba5/8d4e3e7a-7548-43ae-b89c-ae7f3c3ca991/ChatGPT_Image_Jul_29_2025_04_40_06_PM.png?table=block&id=23f0d3e2-bb6d-809c-b004-f23c0e075675&spaceId=0e28e540-810b-4715-b51f-c1ec5f175ba5&expirationTimestamp=1756440000000&signature=JTM4QPxjH628JJMoac-yQONBxRV_u-ov1nVyqaCsi8I&downloadName=ChatGPT+Image+Jul+29%2C+2025%2C+04_40_06+PM.png",
       category: "Essay Scoring",
       pdfUrl: "https://tcsuimqcjsqbfjnoxlxo.supabase.co/storage/v1/object/public/Books%20Novus%20Education/A%20level%20score%20boosting%20phrases%20for%20GP%20essays.pdf"
     },
     {
       id: 6,
       title: "How to score 45/50 for GP Essay",
-      image: "https://img.notionusercontent.com/s3/prod-files-secure%2F0e28e540-810b-4715-b51f-c1ec5f175ba5%2Fc6396b73-d53f-4639-96a7-c588bcb1d92c%2FChatGPT_Image_Jul_29_2025_04_41_44_PM.png/size/w=380?exp=1755681823&sig=JVWB69t9HYZT2OTUF7rumemCy4wGbG_jUewy9P9chk4&id=23f0d3e2-bb6d-802e-916f-c0af7f96a9b3&table=block",
+      image: "https://file.notion.so/f/f/0e28e540-810b-4715-b51f-c1ec5f175ba5/c6396b73-d53f-4639-96a7-c588bcb1d92c/ChatGPT_Image_Jul_29_2025_04_41_44_PM.png?table=block&id=23f0d3e2-bb6d-802e-916f-c0af7f96a9b3&spaceId=0e28e540-810b-4715-b51f-c1ec5f175ba5&expirationTimestamp=1756440000000&signature=kVr_-nadTXX6VVSO3IlYLDOAnWUFzJLVInaO3gqtyPs&downloadName=ChatGPT+Image+Jul+29%2C+2025%2C+04_41_44+PM.png",
       category: "Essay Topics",
       pdfUrl: "https://tcsuimqcjsqbfjnoxlxo.supabase.co/storage/v1/object/public/Books%20Novus%20Education/How%20to%20score%2045_50%20for%20GP%20essay%20.pdf"
     },
     {
       id: 7,
       title: "Top 20 GP Topics 2025 Edition",
-      image: "https://img.notionusercontent.com/s3/prod-files-secure%2F0e28e540-810b-4715-b51f-c1ec5f175ba5%2F5a199368-35b1-4dfd-8221-bdfc60b9d81e%2FChatGPT_Image_Jul_29_2025_04_43_32_PM.png/size/w=380?exp=1755681826&sig=_yXyuyXYxgbeNERjSCBRjty65f3bQOjsDRUtgxVhaIc&id=23f0d3e2-bb6d-8069-afb3-cf04648f37b5&table=block",
+      image: "https://file.notion.so/f/f/0e28e540-810b-4715-b51f-c1ec5f175ba5/5a199368-35b1-4dfd-8221-bdfc60b9d81e/ChatGPT_Image_Jul_29_2025_04_43_32_PM.png?table=block&id=23f0d3e2-bb6d-8069-afb3-cf04648f37b5&spaceId=0e28e540-810b-4715-b51f-c1ec5f175ba5&expirationTimestamp=1756440000000&signature=s9k7AYM1XjRdO-Ols62W7LOkexU91nG-Eg1NlslpaN4&downloadName=ChatGPT+Image+Jul+29%2C+2025%2C+04_43_32+PM.png",
       category: "Revision Tools",
       pdfUrl: "https://tcsuimqcjsqbfjnoxlxo.supabase.co/storage/v1/object/public/Books%20Novus%20Education/Top%2020%20GP%20topics%202025%20edition.pdf"
     },
     {
       id: 8,
       title: "GP Paper 2 Quick Revision Tool Kit",
-      image: "https://img.notionusercontent.com/s3/prod-files-secure%2F0e28e540-810b-4715-b51f-c1ec5f175ba5%2F8caad618-ddf0-47ff-964b-965d520612eb%2FChatGPT_Image_Jul_29_2025_04_45_27_PM.png/size/w=380?exp=1755681828&sig=1NYd6WE2bgk-vdalVZVaQ2kKNjZMFFon4rZ4YJp-FUo&id=23f0d3e2-bb6d-80d4-aec6-ce458e2fe53a&table=block",
+      image: "https://file.notion.so/f/f/0e28e540-810b-4715-b51f-c1ec5f175ba5/8caad618-ddf0-47ff-964b-965d520612eb/ChatGPT_Image_Jul_29_2025_04_45_27_PM.png?table=block&id=23f0d3e2-bb6d-80d4-aec6-ce458e2fe53a&spaceId=0e28e540-810b-4715-b51f-c1ec5f175ba5&expirationTimestamp=1756440000000&signature=joVXXAqVFtyAT2tRB8ZxlgGJ6lDafF2UqQgAOnXtUW8&downloadName=ChatGPT+Image+Jul+29%2C+2025%2C+04_45_27+PM.png",
       category: "Model Essays",
       pdfUrl: "https://tcsuimqcjsqbfjnoxlxo.supabase.co/storage/v1/object/public/Books%20Novus%20Education/GP%20paper%202%20quick%20revision%20tool%20kit.pdf"
     },
     {
       id: 9,
       title: "Model Essays for Climate Change, AI, and Inequality",
-      image: "https://img.notionusercontent.com/s3/prod-files-secure%2F0e28e540-810b-4715-b51f-c1ec5f175ba5%2Fa6a285c4-63c6-412c-9971-d6b5a58a1e7d%2FChatGPT_Image_Jul_29_2025_04_51_41_PM.png/size/w=380?exp=1755681830&sig=cc2aa0sbgyop0kfL1kodV0o6_scePtd1P2ZyF2LXLbc&id=23f0d3e2-bb6d-807d-bef3-f13dcbf9303a&table=block",
+      image: "https://file.notion.so/f/f/0e28e540-810b-4715-b51f-c1ec5f175ba5/a6a285c4-63c6-412c-9971-d6b5a58a1e7d/ChatGPT_Image_Jul_29_2025_04_51_41_PM.png?table=block&id=23f0d3e2-bb6d-807d-bef3-f13dcbf9303a&spaceId=0e28e540-810b-4715-b51f-c1ec5f175ba5&expirationTimestamp=1756440000000&signature=phXQBW8sqweh5TrpLglue7YvqhvF5p7D6Tb0VRU4doE&downloadName=ChatGPT+Image+Jul+29%2C+2025%2C+04_51_41+PM.png",
       category: "Vocabulary",
       pdfUrl: "https://tcsuimqcjsqbfjnoxlxo.supabase.co/storage/v1/object/public/Books%20Novus%20Education/Model%20essays%20for%20climate%20change,%20AI%20and%20inequality%20-%20updated%20for%202025%20topics.pdf"
     },
     {
       id: 10,
       title: "Top 50 Vocabulary in Context Questions",
-      image: "https://img.notionusercontent.com/s3/prod-files-secure%2F0e28e540-810b-4715-b51f-c1ec5f175ba5%2F927890c1-5f4a-4873-8044-9338fc6f349b%2FChatGPT_Image_Jul_29_2025_04_53_48_PM.png/size/w=380?exp=1755681834&sig=SV7HuzZNwjumkLlyPlU303jS7nH8jm2utucmUWoA1ec&id=23f0d3e2-bb6d-8071-b059-c008bb3050a5&table=block",
+      image: "https://file.notion.so/f/f/0e28e540-810b-4715-b51f-c1ec5f175ba5/927890c1-5f4a-4873-8044-9338fc6f349b/ChatGPT_Image_Jul_29_2025_04_53_48_PM.png?table=block&id=23f0d3e2-bb6d-8071-b059-c008bb3050a5&spaceId=0e28e540-810b-4715-b51f-c1ec5f175ba5&expirationTimestamp=1756440000000&signature=gdqpDEZ9zUnZVwPhZoj1TxRs-Vj1vy6mpeuotvbpx6A&downloadName=ChatGPT+Image+Jul+29%2C+2025%2C+04_53_48+PM.png",
       category: "Comprehension",
       pdfUrl: "https://tcsuimqcjsqbfjnoxlxo.supabase.co/storage/v1/object/public/Books%20Novus%20Education/Top%2050%20vocabulary%20in%20context%20questions.pdf"
     },
     {
       id: 11,
       title: "50 Comprehension Traps and How to Outsmart Them",
-      image: "https://img.notionusercontent.com/s3/prod-files-secure%2F0e28e540-810b-4715-b51f-c1ec5f175ba5%2F50781a62-873e-4cc5-8483-ddb49628e67e%2FChatGPT_Image_Jul_29_2025_04_57_12_PM.png/size/w=380?exp=1755681836&sig=fyBmI57blKd4eYhGf6FlCR4tcQ_vdGK8PdwvzcalAIk&id=23f0d3e2-bb6d-8000-8305-f78338fd749b&table=block",
+      image: "https://file.notion.so/f/f/0e28e540-810b-4715-b51f-c1ec5f175ba5/50781a62-873e-4cc5-8483-ddb49628e67e/ChatGPT_Image_Jul_29_2025_04_57_12_PM.png?table=block&id=23f0d3e2-bb6d-8000-8305-f78338fd749b&spaceId=0e28e540-810b-4715-b51f-c1ec5f175ba5&expirationTimestamp=1756440000000&signature=fDqLcrVOn0-u9VZP77uvh5PmOmc78FEml1ENRR7hkUw&downloadName=ChatGPT+Image+Jul+29%2C+2025%2C+04_57_12+PM.png",
       category: "Exam Strategies",
      pdfUrl: "https://tcsuimqcjsqbfjnoxlxo.supabase.co/storage/v1/object/public/Books%20Novus%20Education/50%20comprehension%20traps%20and%20how%20to%20outsmart%20them.pdf" 
       
@@ -92,35 +92,35 @@ export default function ResourcesPage() {
     {
       id: 12,
       title: "Score Fast Think Deep - High Stress Tool Kit",
-      image: "https://img.notionusercontent.com/s3/prod-files-secure%2F0e28e540-810b-4715-b51f-c1ec5f175ba5%2F73af11eb-45f2-40e7-82ef-4292c13e8e4f%2FChatGPT_Image_Jul_30_2025_09_45_34_AM.png/size/w=380?exp=1755681838&sig=-AXjMKuezJFxxV_FiSdG79V-SVUTP-RfkCnZ3ISMXrc&id=2400d3e2-bb6d-80ec-9671-cf875631e53a&table=block",
+      image: "https://file.notion.so/f/f/0e28e540-810b-4715-b51f-c1ec5f175ba5/73af11eb-45f2-40e7-82ef-4292c13e8e4f/ChatGPT_Image_Jul_30_2025_09_45_34_AM.png?table=block&id=2400d3e2-bb6d-80ec-9671-cf875631e53a&spaceId=0e28e540-810b-4715-b51f-c1ec5f175ba5&expirationTimestamp=1756440000000&signature=UbdxoOcn8u4EPLqYQp7NPwHheW4JyFkWJNpr-IJBrbw&downloadName=ChatGPT+Image+Jul+30%2C+2025%2C+09_45_34+AM.png",
       category: "Comprehension",
       pdfUrl: "https://tcsuimqcjsqbfjnoxlxo.supabase.co/storage/v1/object/public/Books%20Novus%20Education/Score%20fast.%20think%20deep%20a%20tool%20kid%20for%20high%20stress%20GP%20paper%202%20situations.pdf"
     },
     {
       id: 13,
       title: "Score 27+ GP Reading Comprehension",
-      image: "https://img.notionusercontent.com/s3/prod-files-secure%2F0e28e540-810b-4715-b51f-c1ec5f175ba5%2F02e766ba-bdc1-482d-9e3b-738553eb24a6%2FChatGPT_Image_Jul_30_2025_10_33_42_AM.png/size/w=380?exp=1755681842&sig=RD8FYTOyq096bnCVGtGWUuEkHQ6L-zFd5wdCs14i9Y8&id=2400d3e2-bb6d-8008-b6d8-e24a4f694c5b&table=block",
+      image: "https://file.notion.so/f/f/0e28e540-810b-4715-b51f-c1ec5f175ba5/02e766ba-bdc1-482d-9e3b-738553eb24a6/ChatGPT_Image_Jul_30_2025_10_33_42_AM.png?table=block&id=2400d3e2-bb6d-8008-b6d8-e24a4f694c5b&spaceId=0e28e540-810b-4715-b51f-c1ec5f175ba5&expirationTimestamp=1756440000000&signature=4Crc_HXsXjaLDCoWbRJN3dotigzJeGzE-7bjL9_92g0&downloadName=ChatGPT+Image+Jul+30%2C+2025%2C+10_33_42+AM.png" ,
       category: "Paper 2",
       pdfUrl: "https://tcsuimqcjsqbfjnoxlxo.supabase.co/storage/v1/object/public/Books%20Novus%20Education/Score%2027+%20GP%20reading%20comprehension.pdf"
     },
     {
       id: 14,
       title: "Break the 30 Mark Barrier",
-      image: "https://img.notionusercontent.com/s3/prod-files-secure%2F0e28e540-810b-4715-b51f-c1ec5f175ba5%2Ff2a2db7b-445b-4e1a-a205-c66a7a9871e9%2FChatGPT_Image_Jul_30_2025_10_38_40_AM.png/size/w=380?exp=1755681843&sig=M_UIEbo6st21-JQEcsJKCramrGJRIR41dZr6hN5M6gA&id=2400d3e2-bb6d-80cd-aaf6-ec9c2b545c6b&table=block",
+      image: "https://file.notion.so/f/f/0e28e540-810b-4715-b51f-c1ec5f175ba5/f2a2db7b-445b-4e1a-a205-c66a7a9871e9/ChatGPT_Image_Jul_30_2025_10_38_40_AM.png?table=block&id=2400d3e2-bb6d-80cd-aaf6-ec9c2b545c6b&spaceId=0e28e540-810b-4715-b51f-c1ec5f175ba5&expirationTimestamp=1756440000000&signature=xke1UVgGcWeUSwaunhV2JhzRc5NZ-FZdI83eyvZTpK0&downloadName=ChatGPT+Image+Jul+30%2C+2025%2C+10_38_40+AM.png",
       category: "Comprehension",
       pdfUrl: "https://tcsuimqcjsqbfjnoxlxo.supabase.co/storage/v1/object/public/Books%20Novus%20Education/Break%20the%2030%20mark%20barrier,%20your%20first%20big%20win%20in%20GP%20paper%202.pdf"
     },
     {
       id: 15,
       title: "The Big List of Common Mistakes in GP Comprehension",
-      image: "https://img.notionusercontent.com/s3/prod-files-secure%2F0e28e540-810b-4715-b51f-c1ec5f175ba5%2F4a96255d-4363-4680-8b3d-a97cdaab988f%2FChatGPT_Image_Jul_30_2025_10_41_28_AM.png/size/w=380?exp=1755681845&sig=71Ib_PT7NV2KInQzw3zXpTie_PzImVindhtLJabmwgw&id=2400d3e2-bb6d-80a5-a316-f47b6cb96aa5&table=block",
+      image: "https://file.notion.so/f/f/0e28e540-810b-4715-b51f-c1ec5f175ba5/4a96255d-4363-4680-8b3d-a97cdaab988f/ChatGPT_Image_Jul_30_2025_10_41_28_AM.png?table=block&id=2400d3e2-bb6d-80a5-a316-f47b6cb96aa5&spaceId=0e28e540-810b-4715-b51f-c1ec5f175ba5&expirationTimestamp=1756440000000&signature=dz-rf13L9krQ_Jl2U8t7m6Ee6OYeW4_xR4pPg-IQFjM&downloadName=ChatGPT+Image+Jul+30%2C+2025%2C+10_41_28+AM.png",
       category: "Essay Topics",
       pdfUrl: "https://tcsuimqcjsqbfjnoxlxo.supabase.co/storage/v1/object/public/Books%20Novus%20Education/The%20big%20list%20of%20common%20mistakes%20in%20GP%20comprehension.pdf"
     },
     {
       id: 16,
       title: "7 Must Know GP Essay Topics",
-      image: "https://img.notionusercontent.com/s3/prod-files-secure%2F0e28e540-810b-4715-b51f-c1ec5f175ba5%2F9df74a64-fec8-4bd7-b125-0fdaa466a54f%2FChatGPT_Image_Jul_31_2025_12_20_31_PM.png/size/w=380?exp=1755681848&sig=maRD9fpA_euQQ1V-YvkOB_tK_2oZB4YCOuudEwKzab4&id=2410d3e2-bb6d-80a7-a62c-dee396d3643e&table=block",
+      image: "https://file.notion.so/f/f/0e28e540-810b-4715-b51f-c1ec5f175ba5/9df74a64-fec8-4bd7-b125-0fdaa466a54f/ChatGPT_Image_Jul_31_2025_12_20_31_PM.png?table=block&id=2410d3e2-bb6d-80a7-a62c-dee396d3643e&spaceId=0e28e540-810b-4715-b51f-c1ec5f175ba5&expirationTimestamp=1756440000000&signature=9nrBUrmqYcESOaL3v7DVHbkQ3gHv7tNBshkdhxoqmTY&downloadName=ChatGPT+Image+Jul+31%2C+2025%2C+12_20_31+PM.png",
       category: "Essay Writing",
       pdfUrl: "https://tcsuimqcjsqbfjnoxlxo.supabase.co/storage/v1/object/public/Books%20Novus%20Education/7%20Must%20Know%20GP%20Essay%20Topics.pdf"
     }
